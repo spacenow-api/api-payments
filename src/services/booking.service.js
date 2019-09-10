3d719bf09455470ee6f79733d9402507556ce01d
Arthemus <arthemus.moreira@gmail.com>
2019-09-10 15:56:57 +1000

    Fixing environment variables.

--- src/services/booking.service.js
+++ src/services/booking.service.js
@@ -7,21 +7,21 @@ const { Transaction } = require('./../models');
 module.exports = {
 
   getBookingById: async (id) => {
-    return axios.get(`${process.env.BOOKINGS_API_HOST}/bookings/${id}`)
+    return axios.get(`${process.env.API_BOOKING}/bookings/${id}`)
   },
 
   onUpdateStateById: async (bookingId, bookingType) => {
     if (!bookingType)
       throw new Error('Booking Type incorrect or missing.')
     if (bookingType === 'request') {
-      return axios.put(`${process.env.BOOKINGS_API_HOST}/bookings/request/${bookingId}`)
+      return axios.put(`${process.env.API_BOOKING}/bookings/request/${bookingId}`)
     } else {
-      return axios.put(`${process.env.BOOKINGS_API_HOST}/bookings/approve/${bookingId}`)
+      return axios.put(`${process.env.API_BOOKING}/bookings/approve/${bookingId}`)
     }
   },
 
   onUpdateBookingChargeAndCard: async (bookingId, cardId, chargeId) => {
-    return axios.put(`${process.env.BOOKINGS_API_HOST}/bookings/${bookingId}`, { sourceId: cardId, chargeId: chargeId })
+    return axios.put(`${process.env.API_BOOKING}/bookings/${bookingId}`, { sourceId: cardId, chargeId: chargeId })
   },
 
   onUpdateTransaction: async (bookingId, chargeId, guestEmail, guestId, hostEmail, hostId, amount, currency) => {
