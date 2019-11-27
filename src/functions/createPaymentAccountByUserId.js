23f3b0490a45bdd24789a93686863766ac2aa87a
Lucas <csalucasnascimento@gmail.com>
2019-11-27 15:53:46 +1100

    adding acceptance on Tos_acceptance for stripe

--- src/functions/createPaymentAccountByUserId.js
+++ src/functions/createPaymentAccountByUserId.js
@@ -5,8 +5,14 @@ const paymentService = require('./../services/payment.service')
 
 module.exports.main = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false
+
+  const tos_acceptance = {
+    "date": Math.floor(Date.now() / 1000),
+    "ip": event.requestContext.identity.sourceIp
+  }
+
   paymentService
-    .createPaymentAccountByUserId(event.pathParameters.userId, JSON.parse(event.body))
+    .createPaymentAccountByUserId(event.pathParameters.userId, { ...JSON.parse(event.body), tos_acceptance })
     .then((data) => callback(null, r.success(data)))
     .catch((err) => callback(null, r.failure(err)))
 }
