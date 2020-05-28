6429b84dbdd92f6e8f34d233fa297259d519609b
Lucas <csalucasnascimento@gmail.com>
2020-05-28 16:05:11 +1000

    remove mandatory

--- src/models/listingPhotos.model.js
+++ src/models/listingPhotos.model.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   return sequelize.define(
     'ListingPhotos',
     {
@@ -29,19 +29,15 @@ module.exports = function(sequelize, DataTypes) {
       },
       bucket: {
         type: DataTypes.STRING(255),
-        allowNull: false
       },
       region: {
         type: DataTypes.STRING(255),
-        allowNull: false
       },
       key: {
         type: DataTypes.STRING(255),
-        allowNull: false
       },
       type: {
         type: DataTypes.STRING(255),
-        allowNull: false
       },
       createdAt: {
         type: DataTypes.DATE,
